e916742469f6f8a59be7ddba6cf77ce1645b2abd
yehezkiel1086 <tsukikagemaikaka1@gmail.com>
2025-08-31 13:41:26 +0700

    chore: reinit project

--- internal/core/domain/error.go
+++
@@ -1,8 +0,0 @@
-package domain
-
-import "errors"
-
-var (
-	ErrAuthRequired = errors.New("Username and password are required")
-	ErrInternal = errors.New("Internal server error")
-)
--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#CompileDaemon -command="./go-hexa-archi"
-
-# build
-go build -o ./tmp/main ./cmd/http/main.go
-
-# run
-./tmp/main
