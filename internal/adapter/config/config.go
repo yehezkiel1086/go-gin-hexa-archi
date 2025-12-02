7fc60ec020484bb7a03b6636939c5c29c96af9cd
yehezkiel1086 <tsukikagemaikaka1@gmail.com>
2025-12-02 14:30:18 +0700

    chore: add go db and general configs

---
+++ internal/adapter/config/config.go
@@ -0,0 +1,70 @@
+package config
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type (
+	Container struct {
+		App  *App
+		HTTP *HTTP
+		DB *DB
+	}
+
+	App struct {
+		Name string
+		Env  string
+	}
+
+	HTTP struct {
+		Host           string
+		Port           string
+		AllowedOrigins string
+	}
+
+	DB struct {
+		Host     string
+		Port     string
+		User     string
+		Password string
+		Name     string
+		TimeZone string
+	}
+)
+
+func New() (*Container, error) {
+	if os.Getenv("APP_ENV") != "production" {
+		err := godotenv.Load()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	App := &App{
+		Name: os.Getenv("APP_NAME"),
+		Env:  os.Getenv("APP_ENV"),
+	}
+
+	HTTP := &HTTP{
+		Host:           os.Getenv("HTTP_HOST"),
+		Port:           os.Getenv("HTTP_PORT"),
+		AllowedOrigins: os.Getenv("HTTP_ALLOWED_ORIGINS"),
+	}
+
+	DB := &DB{
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		User:     os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASSWORD"),
+		Name:     os.Getenv("DB_NAME"),
+		TimeZone: os.Getenv("DB_TIMEZONE"),
+	}
+
+	return &Container{
+		App:  App,
+		HTTP: HTTP,
+		DB: DB,
+	}, nil
+}
